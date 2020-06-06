23a5ea38482788a684e9e4d0e13a082f761fc737
alexanderajju <alexanderajju@gmail.com>
2020-06-06 10:25:48 +0530

    final file

---
+++ first_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    descri = models.TextField()
+    price = models.IntegerField()
+    Offer = models.BooleanField(default=False)
